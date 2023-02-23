0c08d8105b7936b3cd485a91234aabad7690ae83
ICKE\ailee <aileen.schulz@docc.techstarter.de>
2023-02-23 17:58:18 +0100

    JSON konfiguriert

--- Chat/message.js
+++
@@ -1,9 +0,0 @@
-const messageInput = document.getElementById("message-input");
-const sendButton = document.getElementById("send-button");
-const exitButton = document.getElementById("exit-button");
-const messages = document.getElementById("messages");
-
-// Wenn der "Exit"-Button geklickt wird
-exitButton.addEventListener("click", () => {
-    socket.close();
-  });
