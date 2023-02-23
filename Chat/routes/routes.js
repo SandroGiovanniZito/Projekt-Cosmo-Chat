75eada8fe4b7b50156e505eaa8585e3afe31d583
ICKE\ailee <aileen.schulz@docc.techstarter.de>
2023-02-23 21:53:32 +0100

    GET-Method

--- Chat/routes/routes.js
+++ Chat/routes/routes.js
@@ -3,7 +3,7 @@ const userRoutes = require('./users');
 const appRouter = (app, fs) => {
 
 app.get('/', (req, res) => {
-    res.send('welcome to the development api-server');
+    res.send('Server is running on port 8000.');
   });
 
   userRoutes(app, fs);
--- Chat/server.js
+++ Chat/server.js
@@ -16,110 +16,14 @@ const routes = require('./routes/routes.js')(app, fs);
 var PORT = 8000 
 // Start server
 app.listen(PORT, () => {
-    console.log("Server running on port 8000.")
+    console.log("Run")
 });
 // Root endpoint
 app.get("/", (req, res, next) => {
-    res.json({"message":"Ok"})
+    res.json("Server is running on port 8000")
 });
 
 // API endpoints methods
 app.use(function(req, res){
     res.status(404);
 });
-
-app.get("/users", (req, res, next) => {
-    var sql = "select * from user"
-    db.all(sql, (err, rows) => {
-        if (err) {
-          res.status(400).json({"error":err.message});
-          return;
-        }
-        res.json({
-            "message":"success",
-            "data":rows
-        })
-      });
-});
-
-app.get("/user/:id", (req, res) => {
-    var sql = "select * from user where id = ?"
-    var params = [req.params.id]
-    db.get(sql, params, (err, row) => {
-        if (err) {
-          res.status(400).json({"error":err.message});
-          return;
-        }
-        res.json({
-            "message":"success",
-            "data":row
-        })
-      });
-});
-
-app.post("/user/", (req, res, next) => {
-    var errors=[]
-    if (!req.body.password){
-        errors.push("No password specified");
-    }
-    if (!req.body.email){
-        errors.push("No email specified");
-    }
-    if (errors.length){
-        res.status(400).json({"error":errors.join(",")});
-        return;
-    }
-    var data = {
-        name: req.body.name,
-    }
-    var sql ='INSERT INTO user (name) VALUES (?,?,?)'
-    var params =[data.name]
-    db.run(sql, params, function (err, result) {
-        if (err){
-            res.status(400).json({"error": err.message})
-            return;
-        }
-        res.json({
-            "message": "success",
-            "data": data,
-            "id" : this.lastID
-        })
-    });
-})
-
-app.patch("/user/:id", (req, res, next) => {
-    var data = {
-        name: req.body.name,
-        email: req.body.email,
-        password : req.body.password ? md5(req.body.password) : null
-    }
-    db.run(
-        `UPDATE user set 
-           name = COALESCE(?,name), 
-           WHERE id = ?`,
-        [data.name, data.email, data.password, req.params.id],
-        function (err, result) {
-            if (err){
-                res.status(400).json({"error": res.message})
-                return;
-            }
-            res.json({
-                message: "success",
-                data: data,
-                changes: this.changes
-            })
-    });
-})
-
-app.delete("/user/:id", (req, res, next) => {
-    db.run(
-        'DELETE FROM user WHERE id = ?',
-        req.params.id,
-        function (err, result) {
-            if (err){
-                res.status(400).json({"error": res.message})
-                return;
-            }
-            res.json({"message":"deleted", changes: this.changes})
-    });
-})
